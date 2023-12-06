b110eb735f6cf5e76b1d9cbd3a841a6bcb979a33
Quentin <quentin_deni@hotmail.fr>
2023-12-06 11:10:07 +0100

    CPP

--- CPP 09/ex01/RPN.cpp
+++ CPP 09/ex01/RPN.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   RPN.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qdenizar <qdenizar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 09:22:50 by qdenizar          #+#    #+#             */
-/*   Updated: 2023/12/05 15:25:44 by qdenizar         ###   ########.fr       */
+/*   Updated: 2023/12/06 11:05:04 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,77 +46,123 @@
 
     int     RPN::rpn(std::string argv)
     {
-	for (std::string::const_iterator it = argv.begin(); it != argv.end(); it++) {
-		if (*it == ' ')
-			continue;
-		if (*it == '-' || *it == '+' || *it == '/' || *it == '*') {
-			if (_stack.size() >= 2)
-				doOp(*it);
-			continue;
-		}
-		_stack.push(*it - 48);
-	}
-	if (_stack.size() > 1) {
-		return (8);
-	}
-	std::cout << "Result: " << _stack.top() << std::endl;
-    return (0);
+        int i = 0;
+	    for (std::string::const_iterator it = argv.begin(); it != argv.end(); it++)
+        {
+		    if (*it == ' ')
+			    continue;
+		    if (*it == '-' || *it == '+' || *it == '/' || *it == '*')
+            {
+			    if (_stack.size() >= 2)
+                {
+				    i = doOp(*it);
+                    if (i != 0)
+                    {
+                        error(i);
+                        return (1);
+                    }
+                }
+			    continue;
+		    }
+		    _stack.push(*it - 48);
+	    }
+	    if (_stack.size() > 1)
+		    return (8);
+	    std::cout << "\033[0;36mResult: \033[1m" << _stack.top() << "\033[0m" << std::endl;
+        return (0);
     }
     
     int    RPN::doOp(const char ope)
     {
-        int a = _stack.top();
+        int topNumber = _stack.top();
 	    _stack.pop();
-	    int b = _stack.top();
+	    int firstToBePush = _stack.top();
 	    _stack.pop();
 	    switch(ope) {
 		case '+':
-			_stack.push(b + a);
-			break;
+        {
+			_stack.push(firstToBePush + topNumber);
+			break;    
+        }
 		case '-':
-			_stack.push(b - a);
+        {
+			_stack.push(firstToBePush - topNumber);
 			break;
+        }
 		case '*':
-			_stack.push(b * a);
+        {
+			_stack.push(firstToBePush * topNumber);
 			break;
+        }
 		case '/':
-			_stack.push(b / a);
-			break;
+        {
+            if (topNumber == 0)
+                return (4); //division by zero
+			_stack.push(firstToBePush / topNumber);
+			break;   
+        }
 	}
     return (0);
     }
 
     int     RPN::parse(const std::string argv)
     {
+        int compNumber = 0;
+        int compOperator = 0;
+        if (argv.length() == 0)
+            return (3);
         for (int i = 0; i < int(argv.length()); i++)
         {
-		if (i % 2 == 1)
+            if (argv[0] == ' ')
+                return (7);
+            if (argv[i] == '-' || argv[i] == '+' || argv[i] == '/' || argv[i] == '*' || argv[i] == ' ' || (argv[i] >= '0' && argv[i] <= '9'))
+                ;
+            else
+            {
+               std::cout << "argv[i] = [" << argv[i] << "]" << std::endl; 
+			    return (1);
+            }
+		    if (i % 2 == 1)
+            {
+			    if (argv[i] != ' ')
+				    return (7);
+                if (argv[i] == ' ' && argv[i + 1] == ' ')
+                    return (7);
+		    }
+            if (argv[i] >= '0' && argv[i] <= '9')
+                compNumber++;
+            if (argv[i] == '-' || argv[i] == '+' || argv[i] == '/' || argv[i] == '*')
+                compOperator++;
+            if (argv[i] >= '0' && argv[i] <= '9' && argv[i + 1] >= '0' && argv[i + 1] <= '9')
+                return (6);
+            if (i == 3 && compNumber != 2)
+                return (2);
+	    }
+        if (compNumber - compOperator != 1)
         {
-			if (argv[i] != ' ')
-				return (7);
-		}
-        else if (argv[i] != '-' && argv[i] != '+' && argv[i] != '/' && argv[i] != '*' && !isdigit(argv[i]))
-			return (1);
-	}
-	return 0;
+            return (8);
+        }
+	    return (0);
     }
     
     void	RPN::error(int i)
     {
         if (i == 1)
-            std::cout << "Error: invalid character" << std::endl;
+            std::cout << "\033[0;31mError: invalid character\033[0m" << std::endl;
         else if (i == 2)
-            std::cout << "Error: stack is empty" << std::endl;
+            std::cout << "\033[0;31mError: need at least two numbers at the begining\033[0m" << std::endl;
         else if (i == 3)
-            std::cout << "Error: not enaf elements" << std::endl;
+            std::cout << "\033[0;31mError: not enaf elements\033[0m" << std::endl;
         else if (i == 4)
-            std::cout << "Error: division by zero" << std::endl;
+            std::cout << "\033[0;31mError: division by zero\033[0m" << std::endl;
         else if ( i == 5)
-            std::cout << "Error: where is the operator ?" << std::endl;
+            std::cout << "\033[0;31mError: where is the operator ?\033[0m" << std::endl;
         else if (i == 6)
-            std::cout << "Error: can't manage positif number" <<std::endl;
+            std::cout << "\033[0;31mError: can't manage number > 9\033[0m" <<std::endl;
         else if (i == 7)
-            std::cout << "Error: please take care put space between " <<std::endl;
+            std::cout << "\033[0;31mError: please take care put one space between things\033[0m" <<std::endl;
         else if (i == 8)
-            std::cout << "Error: number walking alone in the string" <<std::endl;
+            std::cout << "\033[0;31mError: number or operator walking alone in the string\033[0m" <<std::endl;
+        else if (i == 9)
+            std::cout << "\033[0;31mError: Please don't use SPACE at first character\033[0m" <<std::endl;
     }
--- CPP 09/ex01/main.cpp
+++ CPP 09/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qdenizar <qdenizar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 09:22:53 by qdenizar          #+#    #+#             */
-/*   Updated: 2023/12/05 15:48:07 by qdenizar         ###   ########.fr       */
+/*   Updated: 2023/12/06 10:51:46 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,21 @@ int    main(int argc, char **argv)
     int i =0;
     if (argc != 2)
     {
-        std::cout << "Usage: ./RPN \"Inverse polish notation\"" << std::endl;
+        std::cout << "\033[0;33mUsage: ./RPN \"Inverse polish notation\"\033[0m" << std::endl;
         return (1);
     }
-    //std::cout << "argv[1] = " << argv[1] << std::endl; 
+    std::cout << "\033[0;32m/*-+/*-+/*-+/*-+/*-+/*-+/*-+/*-+/*-+/*-+*-+/" << std::endl;
+     std::cout << "/ \033[0;35m         WELCOME DEAR USER              \033[0;32m/" << std::endl; 
+    std::cout << "/ \033[0;35mThis RPN manage number between 0 and 9 \033[0;32m/" << std::endl; 
+    std::cout << "/*-+/*-+/*-+/*-+/*-+/*-+/*-+/*-+/*-+/*-+/\033[0m" << std::endl;
     RPN rpn;
     i = rpn.parse(argv[1]);
-    rpn.rpn(argv[1]);
     if (i != 0)
     {
         rpn.error(i);
         return (1);
     }
-    return (0);
+    else
+        i = rpn.rpn(argv[1]);
+    return (i);
 }
